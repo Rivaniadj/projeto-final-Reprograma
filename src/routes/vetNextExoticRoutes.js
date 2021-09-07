ecc45e753181ae51acd9e0be119f2fc5aa628913
Rivania Silva <rimieli@hotmail.com>
2021-09-07 10:40:07 -0300

    atualizando router

--- src/routes/vetNextExoticRoutes.js
+++ src/routes/vetNextExoticRoutes.js
@@ -2,14 +2,16 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/vetNextExoticController')
 
+router.get("/oi", (req, resp)=>{
+    resp.status(200).send({"mensagem":"oi to aqui ta funcionando "})
+})
+
 router.get('/', controller.getAll);
 router.get("/especialidade/:especialidade", controller.getEspecialidade);
 router.post('/', controller.createVetNextExotic);
 router.get("/:id", controller.getId);
 
-router.get("/oi", (req, resp)=>{
-    resp.status(200).send({"mensagem":"oi to aqui ta funcionando "})
-})
+
 
 
 module.exports = router
