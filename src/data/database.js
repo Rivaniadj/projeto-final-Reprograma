332effe81b269e0256db33da978eee8e240d59a0
Rivania Silva <rimieli@hotmail.com>
2021-09-07 00:24:12 -0300

    atualizando o database

--- src/data/database.js
+++ src/data/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 require("dotenv").config();
 
-const MONGODB = process.env.MONGODB_URI || process.env.MONGODB_URL
+const MONGODB = process.env.MONGODB_URI 
 
 const connect = () => {mongoose.connect(
   MONGODB, {
