58291c4913d98e4b790c00faa5a9fbccf8f83260
Rivania Silva <rimieli@hotmail.com>
2021-09-07 00:55:50 -0300

    mudando a senha

--- app/src/routes/administradorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/administradorController')
-
-router.post('/create', controller.create)
- router.post('/login', controller.login)
-
-module.exports = router
